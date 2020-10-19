b26863d2bd8d10705ebb9b7ef2767c9b8dcbf803
ahlamls <fulldoang@gmail.com>
2020-10-19 14:47:07 +0700

    tampilan komen

---
+++ application/views/home.php
@@ -0,0 +1,193 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<style>
+.pad5 {
+    padding:5px;
+}
+.padkomen {
+  padding-left :15px;
+  padding-right:15px;
+  padding-top:2px;
+  padding-bottom:2px;
+}
+</style>
+    <title>asede</title>
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-md navbar-dark bg-danger">
+  <a class="navbar-brand" href="#">
+    <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+    asede
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/recent">Fresh</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/legendary">Mythic</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/shuffle">Random</a>
+      </li>
+    
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+    <div class="container">
+<br>
+    <div class="alert alert-success" role="alert">
+    Download Aplikasi asede untuk Android (Termasuk Burn in dan jamur)
+</div>
+
+<div class="row">
+
+<div class="col-12 col-md-8 pad5">
+
+<div class="card w-100" >
+<div class="card-header">
+    <b>DukungFF</B> <span class="text-muted">00-00-0000 00:00:00</span>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">FF vs Bensin Impek</h5>
+    <p class="card-text">game baru dari mihotod pengalihan isu . tetap kawal 8-2 . eh free fire</p>
+    
+  </div>
+  <img src="/mim/ff.jpg" class="card-img-top" alt="FF vs Bensin Impek">
+  <div class="card-body">
+ <p class="card-text">Sumber : Facebook dukung ff<br> <span class="text-muted">2 Vote</span></p>
+    
+    <div class="row">
+    <div class="col-4">
+    <a href="#" class="btn btn-primary w-100">Vote</a>
+    </div>
+    <div class="col-4">
+    <a href="#" class="btn btn-primary w-100">Vote'nt</a>
+    </div>
+    <div class="col-4">
+    <a href="#" class="btn btn-secondary w-100" data-toggle="modal" data-target="#komenModal">Komen</a>
+    </div>
+   
+   
+    </div>
+  </div>
+  
+</div>
+<br>
+
+<a href="#" class="btn btn-primary w-100">Selanjutnya</a>
+
+</div>
+
+<div class="col-12 col-md-4 pad5">
+<hr>
+<h3>Tentang asede</h3>
+<p>
+asede adalah situs untuk berbagi meme tanpa rasa khawatir di report , begal mark , grup/page angud seperti di platform lain. kami mendukung free speech atau bebas untuk memberikan pendapat
+</p>
+    <hr>
+    <p>Kopirek asede 2020 | <a href="/page/about">Tentang</a> - <a href="/page/terms">Terms</a> - <a href="/page/privacy">Privacy</a> - <a href="/page/advertise">Advertise</a></p>
+    </p>
+</div>
+
+</div>
+
+
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+
+    <!-- modol -->
+    <!-- Modol komen -->
+<div class="modal fade" id="komenModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header text-white bg-danger">
+        <h5 class="modal-title " id="exampleModalLabel">Komentar "FF vs Bensin Impek"</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="komenBody">
+
+      <form class="row">
+  <div class="form-group col-9">
+    
+    <input type="text" placeholder="Isi Komentar" class="form-control" id="staticEmail2" >
+  </div>
+  <div class="form-group col-3">
+    
+  <button type="submit" class="btn btn-success w-100">Kirim</button>
+   </div>
+<hr>
+<div class="padkomen">
+<p><b>HenryNasution</b> <span class="text-muted" >00:00:00 00-00-0000</span><br>
+Ngaco Kamu
+
+</div>
+  
+<div class="padkomen">
+<p><b>RoniHartono</b> <span class="text-muted" >00:00:00 00-00-0000</span><br>
+Kamu Bohong
+
+</div>
+
+</form> 
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+  </body>
+</html>
